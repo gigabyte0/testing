68572ea29f2d73b4bf9d10b9d6d541838449a063
0x69 <72536933+gigabyte0@users.noreply.github.com>
2022-04-24 20:07:30 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/service/',
+    prefix: '/testing/service/',
     bare: 'https://api.astralofficial.gq/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
